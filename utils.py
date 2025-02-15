5115a5974d953b1f71f8230f1d0c27aba50e7849
artursil <artur.silicki@gmail.com>
2025-02-15 18:18:53 +0100

    adding utils

---
+++ utils.py
@@ -0,0 +1,29 @@
+def clean_ipython_hist():
+    # Code in this function mainly copied from IPython source
+    if not 'get_ipython' in globals(): return
+    ip = get_ipython()
+    user_ns = ip.user_ns
+    ip.displayhook.flush()
+    pc = ip.displayhook.prompt_count + 1
+    for n in range(1, pc): user_ns.pop('_i'+repr(n),None)
+    user_ns.update(dict(_i='',_ii='',_iii=''))
+    hm = ip.history_manager
+    hm.input_hist_parsed[:] = [''] * pc
+    hm.input_hist_raw[:] = [''] * pc
+    hm._i = hm._ii = hm._iii = hm._i00 =  ''
+
+# %% ../nbs/11_initializing.ipynb 12
+def clean_tb():
+    # h/t Piotr Czapla
+    if hasattr(sys, 'last_traceback'):
+        traceback.clear_frames(sys.last_traceback)
+        delattr(sys, 'last_traceback')
+    if hasattr(sys, 'last_type'): delattr(sys, 'last_type')
+    if hasattr(sys, 'last_value'): delattr(sys, 'last_value')
+
+# %% ../nbs/11_initializing.ipynb 13
+def clean_mem():
+    clean_tb()
+    clean_ipython_hist()
+    gc.collect()
+    torch.cuda.empty_cache()
